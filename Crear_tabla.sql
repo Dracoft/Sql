899869c292bbab5562a6cbdbdafb72ad4bde9eec
Incógnita X <julian.cortes43@gmail.com>
2021-01-03 15:33:59 -0500

    Update Crear_tabla.sql

--- Crear_tabla.sql
+++ Crear_tabla.sql
@@ -1,3 +1,6 @@
 CREATE TABLE producto (idProducto INT NOT NULL) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+//Modificar la tabla
 ALTER TABLE producto RENAME productos;
 RENAME TABLE productos to producto, producto to productos;
+//Eliminar tabla
+DROP TABLE productos
