8e802af19b9a46f33cb1e3e6a9545b797af337a1
Incógnita X <julian.cortes43@gmail.com>
2021-01-09 16:59:38 -0500

    Update Fecha.sql

--- Fecha.sql
+++ Fecha.sql
@@ -5,3 +5,12 @@ SELECT NOW();
 SELECT LOCALTIME();
 
 SELECT STR_TO_DATE('09 Aug, 2020', '%d %m, %y');
+
+SELECT DATE('2020-08-07') AS fecha;
+
+SELECT DATE(NOW())) AS fecha;
+
+SELECT DATEDIFF('2020-08-07', '2019-08-07'); //Diferencia entre fechas
+            
+SELECT nombre, floor(dateiff(now(), fechaNacimiento)/365) AS años 
+FROM Empleados;
