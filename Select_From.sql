933d8a425674c4721888a427d9e5cc7bef70a1f0
Incógnita X <julian.cortes43@gmail.com>
2021-01-09 16:07:30 -0500

    Update Select_From.sql

--- Select_From.sql
+++ Select_From.sql
@@ -1,8 +1,2 @@
 SELECT * FROM nomtabla; //Selecionar todos los campos
 SELECT nomcampo FROM nomtabla; //Selecionar un cmapo espesifico
-
-SELECT nomcampo FROM nomtabla;
-WHERE IdPersona IN(1,3,5,8);
-
-SELECT nomcampo FROM nomtabla;
-WHERE IdPersona NOT IN(1,3,5,8);
