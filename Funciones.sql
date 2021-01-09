7c93678d1e8e1b724b44ad4114ab5bc1d43babcc
Incógnita X <julian.cortes43@gmail.com>
2021-01-09 17:07:26 -0500

    Update Funciones.sql

--- Funciones.sql
+++ Funciones.sql
@@ -1,1 +1,17 @@
+SELECT ABS(6); //Valor absoluto
 
+SELECT ceil(8.75);//Redondear un valor hacia arriba
+
+SELECT floor(9.95);//Volver a entero
+
+SELECT mod(19,5);//Devolver el residuo de la division
+
+SELECT PI();//Muestra el valor de pi
+
+SELECT power(3,2);//Elevar un valor 3 elevado a la 2
+
+SELECT rand();//Valor aleatorio
+
+SELECT round(16.5)//Aplicar el redondeado 
+
+SELECT truncate(5.432, 2);//Cortar decimales
