cee91ff05e686a125cd46a71c50d4002ab8c59c4
Incógnita X <julian.cortes43@gmail.com>
2021-01-09 16:09:06 -0500

    Update Wahere.sql

--- Wahere.sql
+++ Wahere.sql
@@ -1,3 +1,17 @@
 SELECT * FROM NOMTABLA WHERE IdPersona = 2;
 SELECT * FROM NOMTABLA WHERE Pais = 'Estados Unidos';
 SELECT * FROM NomBase.Tabla;
+
+
+SELECT nomcampo FROM nomtabla;
+WHERE IdPersona IN(1,3,5,8);
+
+SELECT nomcampo FROM nomtabla;
+WHERE IdPersona NOT IN(1,3,5,8);
+
+SELECT nomcampo FROM nomtabla;
+WHERE IdPersona IS NULL; //VER LO VALORES NULOS 
+
+SELECT * FROM nomtabla;
+WHERE ISNULL(NomCampo); //VER LO VALORES NULOS 
+
