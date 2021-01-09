78687aa2e3a8eba752ca964ce23643bf874ed53d
Incógnita X <julian.cortes43@gmail.com>
2021-01-09 16:38:44 -0500

    Update O_Arimeticos.sql

--- O_Arimeticos.sql
+++ O_Arimeticos.sql
@@ -1,1 +1,15 @@
+SELECT * FROM tienda.detalle_factura;
 
+SELECT IdProducto,
+precioUnitario = cantidad as total 
+from detalle_factura;
+
+SELECT * FROM tienda.detalle_factura;
+
+SELECT IdProducto,
+(precioUnitario = cantidad) - (preciounitario * cantidad * 0.10) as 'Precio con descuento' 
+from detalle_factura;
+
+SELECT IdProducto,
+(precioUnitario = cantidad) + (preciounitario * cantidad * 0.10) as 'Precio con impuesto' 
+from detalle_factura;
