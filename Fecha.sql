a614a151fb8f63c5b3b2d821b54249ea3abbabfd
Incógnita X <julian.cortes43@gmail.com>
2021-01-09 19:37:54 -0500

    Update Fecha.sql

--- Fecha.sql
+++ Fecha.sql
@@ -9,6 +9,8 @@ SELECT STR_TO_DATE('09 Aug, 2020', '%d %m, %y');
 SELECT DATE('2020-08-07') AS fecha;
 
 SELECT DATE(NOW())) AS fecha;
+            
+SELECT TIMESTAMP();
 
 SELECT DATEDIFF('2020-08-07', '2019-08-07'); //Diferencia entre fechas
             
