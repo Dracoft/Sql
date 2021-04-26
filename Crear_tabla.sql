fb043992fd92b89129dada9b8fe5f4f562352290
Incógnita X <julian.cortes43@gmail.com>
2021-04-26 12:52:28 -0500

    Update Crear_tabla.sql

--- Crear_tabla.sql
+++ Crear_tabla.sql
@@ -4,3 +4,5 @@ ALTER TABLE producto RENAME productos;
 RENAME TABLE productos to producto, producto to productos;
 //Eliminar tabla
 DROP TABLE productos;
+COPIA O BACKUP DE UNA ENTIDAD
+CREATE TABLE BKP_PRUDUCTOS AS SELECT * FROM PRODUCTOS;
